4244ad52dbdb34743194723fcafe6a8661e75310
Harini Mode <113983232+harinimode@users.noreply.github.com>
2025-07-09 10:20:26 +0530

    Add files via upload

---
+++ FinalInspectionResults.sql
@@ -0,0 +1,60 @@
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "FinalInspectionResults" (
+	"Device ID"	TEXT,
+	"Batch ID"	TEXT,
+	"Timestamp"	TEXT,
+	"RoHSCompliance"	TEXT,
+	"Inspection Result"	TEXT,
+	"Failed Reason"	TEXT
+);
+INSERT INTO "FinalInspectionResults" VALUES ('EF6E319B','10E5FC','21-06-2025 15:05:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('98B70A2A','1118AB','21-06-2025 15:05:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('049088CD','15C215','21-06-2025 15:05:00','yes','REJECTED','Blurry Label or Mismatch');
+INSERT INTO "FinalInspectionResults" VALUES ('BEAC72AC','21224','21-06-2025 15:06:00','yes','REJECTED','Blurry Label or Mismatch');
+INSERT INTO "FinalInspectionResults" VALUES ('1A19E59A','23EA89','21-06-2025 15:06:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('1375A781','2E837A','21-06-2025 15:06:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('9AE67CC3','34EA8B','21-06-2025 15:06:00','yes','REJECTED','Blurry Label or Mismatch');
+INSERT INTO "FinalInspectionResults" VALUES ('8F613DE3','37271A','21-06-2025 15:05:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('9EE56D16','372CAB','21-06-2025 15:06:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('BA23A7BA','38D682','21-06-2025 15:06:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('E7DD120A','3F8A99','21-06-2025 15:05:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('6DCBAB9F','476AD4','21-06-2025 15:05:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('3441F704','548077','21-06-2025 15:05:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('22F22873','55A404','21-06-2025 15:05:00','yes','REJECTED','Blurry Label or Mismatch');
+INSERT INTO "FinalInspectionResults" VALUES ('AD702784','621978','21-06-2025 15:05:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('32B7F9C2','65AC13','21-06-2025 15:05:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('DCB80AF9','69232','21-06-2025 15:05:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('1B9512FA','6C53DD','21-06-2025 15:05:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('80C7435B','6E0BFA','21-06-2025 15:05:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('2FC18128','738105','21-06-2025 15:05:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('759DCBC3','7B9D13','21-06-2025 15:08:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('56D340A5','92E02C','21-06-2025 15:08:00','yes','REJECTED','Defective Product Detected');
+INSERT INTO "FinalInspectionResults" VALUES ('6075D2C5','96425E','21-06-2025 15:07:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('E7A9F12B','980035','21-06-2025 15:07:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('60BBA503','9B8F39','21-06-2025 15:08:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('B978A9EC','A11A12','21-06-2025 15:07:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('3D2C4DE1','A16480','21-06-2025 15:08:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('A054107A','A2AE1A','21-06-2025 15:07:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('939C75E7','A43516','21-06-2025 15:08:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('EE42B543','A8F4A7','21-06-2025 15:07:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('1497086F','AAB856','21-06-2025 15:07:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('4B94321A','AD8F6C','21-06-2025 15:07:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('7BDBF8A3','B1807A','21-06-2025 15:07:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('F189863B','B3BBC4','21-06-2025 15:07:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('1D3DD4A0','B45426','21-06-2025 15:06:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('14284285','B7CB51','21-06-2025 15:07:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('311059AD','B9B33A','21-06-2025 15:06:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('0349E677','C7DEE1','21-06-2025 15:06:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('9E2D3FBE','CA1083','21-06-2025 15:06:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('21E97992','CE2FD2','21-06-2025 15:06:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('AAA6E663','CF1321','21-06-2025 15:06:00','yes','REJECTED','Blurry Label or Mismatch');
+INSERT INTO "FinalInspectionResults" VALUES ('AD9B5BDA','CFB31E','21-06-2025 15:08:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('BAFEAEB1','D81A64','21-06-2025 15:08:00','yes','REJECTED','Defective Product Detected');
+INSERT INTO "FinalInspectionResults" VALUES ('B87ED7BD','DA0A38','21-06-2025 15:08:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('24DCA9BF','DAE5E1','21-06-2025 15:09:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('79049BAE','E88C79','21-06-2025 15:08:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('21E9FA39','F02A8B','21-06-2025 15:08:00','yes','APPROVED','');
+INSERT INTO "FinalInspectionResults" VALUES ('185E2CE0','F1BB2B','21-06-2025 15:08:00','yes','REJECTED','Defective Product Detected');
+INSERT INTO "FinalInspectionResults" VALUES ('2188305','0D380A','28-06-2025 18:04','no','REJECTED','Hazardous Substances Found');
+INSERT INTO "FinalInspectionResults" VALUES ('B1032AED','1936D0','28-06-2025 18:04','no','REJECTED','Hazardous Substances Found');
+COMMIT;
